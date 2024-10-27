b7632f54ba0094ab57b362985149b13772e32e0f
AndreiHome <andrei.preoteasa.ap@gmail.com>
2024-10-27 16:28:53 +0200

    one scroll format

--- taru-site/src/App.js
+++ taru-site/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Games from './pages/Games';
 import Contact from './pages/Contact';
+// import './styles.css';
 
-const App = () => (
-  <Router>
+const App = () => {
+  return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/games" element={<Games />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <section id="home">
+        <Home />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="games">
+        <Games />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
       <Footer />
     </div>
-  </Router>
-);
+  );
+};
 
 export default App;
--- taru-site/src/components/Navbar.js
+++ taru-site/src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Navbar = () => (
   <nav className="navbar">
     <h1>Taru Games</h1>
     <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/games">Games</Link></li>
-      <li><Link to="/contact">Contact</Link></li>
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#games">Games</a></li>
+      <li><a href="#contact">Contact</a></li>
     </ul>
   </nav>
 );
