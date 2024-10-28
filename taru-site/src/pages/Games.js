525e6f328ada0f4cd558a9c438a410658f0bc094
AndreiHome <andrei.preoteasa.ap@gmail.com>
2024-10-28 20:58:22 +0200

    adding space buckaroo to the page

--- taru-site/src/pages/Games.js
+++ taru-site/src/pages/Games.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import GameCard from '../components/GameCard';
+import sbTitle from '../img/sbTitle.png'
 
 const Games = () => (
   <div className="games">
     <h1>Our Games</h1>
-    <GameCard title="Adventure Quest" description="An epic RPG adventure." imgSrc="adventure.jpg" />
+    <GameCard title="Adventure Quest" description="An epic RPG adventure." imgSrc=sbTitle />
     <GameCard title="Mystic Realms" description="A magical journey through enchanted lands." imgSrc="mystic.jpg" />
     <GameCard title="Skybound Saga" description="Soar through the skies in a world of floating islands." imgSrc="skybound.jpg" />
   </div>
