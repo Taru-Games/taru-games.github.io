360855aa5947f33466e7b477cb253c1159dd87ba
Owlaboy <andrei.preotesa.ap@gmail.com>
2025-01-20 12:32:07 +0200

    Updated the whole website to be better

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-
-
 import React from 'react';
 import './App.css';
 
+import Header from './components/Header.js';
+import Content from './components/Content.js';
+import {Contact, Address} from './components/Contact.js';
+
+
 function App() {
   return (
-    <div className="App" style={styles.container}>
-      <h1 style={styles.text}>Helsinki-based startup developing AI software tools synthesizing human behavioral data.
-
-</h1>
+    <div className="App">
+      <Header />
+      <div className='top'>
+        <p>AI-synthesized human data for game developers to drive player engagement.</p>
+      </div>
+      <main>
+        <Content />
+        <Address />
+      </main>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#282c34',
-    color: 'white',
-    margin: 0,
-  },
-  text: {
-    fontSize: '3rem',
-    fontFamily: 'Arial, sans-serif',
-  },
-};
 
 export default App;
